65eca0a89006ef32dce69d8ba586010548f40c2c
tim_curchod <timofeyc@hotmail.com>
2025-03-25 17:40:38 +0900

    use SUPABASE_URL

--- drf_two/settings.py
+++ drf_two/settings.py
@@ -158,7 +158,7 @@ if 'DEV' in os.environ:
     }
 else:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        'default': dj_database_url.parse(os.environ.get('SUPABASE_URL'))
     }
 
 
